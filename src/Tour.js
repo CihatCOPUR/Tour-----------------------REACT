60e60b2c217dcdeaaeeb2c13d0b1c0773a321d60
Cihat <copurcihat@gmail.com>
2022-12-20 11:57:16 +0300

    1

--- src/Tour.js
+++ src/Tour.js
@@ -1,15 +1,29 @@
 
-import TourData from './TourData';
+import { Container } from 'react-bootstrap';
+
 
 const Tour = ({ tours}) => {
-console.log(tours)
+
   
   
-  return <h2>
-    { tours.map((tour)=>{
-      return < TourData key = {tour.id} tours= {tours} /> 
-    }) }
-  </h2>;
+  return <div>
+    <h1></h1>
+    {tours.map((tour)=>{
+      return(<Container className='single-tour' key={tour.id}>
+        <img className='.single-tour'  img src={tour.image} alt="" />
+        <Container className='tour-info'>
+        <h3>{tour.name}</h3>
+        <h3>${tour.price}</h3>
+        </Container>
+        <h4 className='info'>{tour.info}</h4>
+
+
+      </Container>
+        
+      )
+    })}
+   
+  </div>;
 };
 
 export default Tour;
